6228822d7a8bf851736b9c0a0236102e27b14b6c
Katarzyna Lisiecka <lisiecka.dev@gmail.com>
2025-01-14 21:33:54 +0100

    Touchup in main to login smoothly for other users

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart'; // Dodano Firebase Core
+import 'package:firebase_core/firebase_core.dart';
+import 'package:lisiecka_aplikacje_mobilne/data/note_database.dart';
 import 'package:lisiecka_aplikacje_mobilne/views/home/home_view.dart';
 import 'package:lisiecka_aplikacje_mobilne/views/splashscreen/splash_screen.dart';
 import 'package:lisiecka_aplikacje_mobilne/views/login/login_view.dart';
@@ -11,6 +12,7 @@ void main() async {
   // Inicjalizujemy Firebase
   await Firebase.initializeApp();
   await testFirebaseConnection();
+  await NoteDatabase.instance.initializeTestData();
 
   runApp(const MyApp());
 }
